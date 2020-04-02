bb36b09a5c88f26c0adc0ccffb9846909702b178
kirin <kirin.chen1001@gmail.com>
2020-04-02 23:51:59 +0800

    add selemkit

--- spider/EtoroSpider.ts
+++ spider/EtoroSpider.ts
@@ -1,4 +1,12 @@
+import { SeleniumKit } from 'selenium-kit';
+import { Config } from 'bzk';
+
 
 export class EtoroSpider {
+    spiderKit: SeleniumKit = new SeleniumKit();
+
+    public async init(c: Config) {
+        //this.spiderKit.init(c.get());
+    }
 
 }
